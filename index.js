bcad4e1f4ffaf304beac787a3fdec9068a4b85b4
碎碎酱 <yinxin630@gmail.com>
2018-07-10 23:57:19 +0800

    feat: 对html响应添加eruda工具

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-module.exports = class Plugin {
+const Koa = require('koa');
+
+module.exports = class DebugToolPlugin {
     proxy() {
         return async (ctx, next) => {
             await next();
+
+            const contentType = ctx.res.getHeader('content-type');
+            if (contentType && !/html/.test(contentType)) {
+                return;
+            }
+
+            const getBodyPromise = new Promise((resolve) => {
+                let body = '';
+                ctx.res.body.on('data', buffer => {
+                    body += buffer.toString();
+                });
+                ctx.res.body.on('end', () => {
+                    resolve(body)
+                });
+            })
+
+            const body = await getBodyPromise;
+            const inject = '<script src="https://cdn.bootcss.com/eruda/1.4.4/eruda.min.js"></script><script>eruda.init()</script>';
+            let index = body.indexOf('<head>');
+            if (index !== -1) {
+                const result = body.slice(0, index + 6) + inject + body.slice(index + 6, body.length);
+                ctx.res.body = result;
+                return;
+            }
+
         }
+    }
+    manage() {
+        return new Koa();
     }
 }
