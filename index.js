40c1899ed64bdd5dc1ae80f8c1fea3c2281a0d42
f48vj <i.am.f48vj+github@gmail.com>
2019-05-06 19:07:45 +0800

    feat: 更换 eruda CDN 自动使用最近版本

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require('vorlon');
 const zpConfigPath = `${os.homedir()}/.front-end-proxy`;
 const configFilePath = `${zpConfigPath}/zp-debug-tool.config.js`;
 
-const erudaScript = '\n<script src="https://cdn.bootcss.com/eruda/1.4.4/eruda.min.js"></script><script>eruda.init()</script>\n';
+const erudaScript = '\n<script src="https://unpkg.com/eruda/eruda.min.js"></script><script>eruda.init()</script>\n';
 
 module.exports = class DebugToolPlugin {
     /**
